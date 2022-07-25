d36050992e54db65ddb5aec107ec538554a00f64
RMPV09 <mauupvillegas2000@hotmail.com>
2022-07-25 13:10:16 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Examen_II_B95811.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
